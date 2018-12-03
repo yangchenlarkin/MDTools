529ac731ef6dc187dc50166ba0ef375895f2c3be
Larkin <yangchenlarkin@gmail.com>
2018-12-03 17:12:50 +0800

    添加了面向切面的测试用例

---
+++ MDToolsDemoTests/MDAspects/TMDAspects_testObject.h
@@ -0,0 +1,19 @@
+//
+//  TMDAspects_testObject.h
+//  MDToolsDemoTests
+//
+//  Created by Larkin Yang on 2018/12/3.
+//  Copyright © 2018 Larkin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TMDAspects_testObject : NSObject
+
+- (void)methodForTesting;
+
+@end
+
+NS_ASSUME_NONNULL_END
